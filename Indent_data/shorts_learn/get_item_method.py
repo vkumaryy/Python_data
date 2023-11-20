3b0c7a62a8b6011cb727e31fbf1d1bae98046d50
Vikash kumar <vkumaryy@gmail.com>
2023-11-20 23:06:57 +0530

    explain Book function

--- Indent_data/shorts_learn/get_item_method.py
+++ Indent_data/shorts_learn/get_item_method.py
@@ -1,5 +1,16 @@
 class Book:
-    def __init__(self,content):
+    """
+    A class representing a book.
+
+    Attributes:
+    - content (list): a list of strings representing the pages of the book.
+
+    Methods:
+    - __init__(self, content): Initializes a Book instance with the given content.
+    - __getitem__(self, index): Retrieves the page at the given index. If the index is out of range, returns 'PAGE not found..!'.
+    """
+
+    def __init__(self, content):
         self.content = content
 
     def __getitem__(self, index):
@@ -8,6 +19,6 @@ class Book:
         except IndexError:
             page = 'PAGE not found..!'
         return page
-    
+
 book = Book(['Eggs','Spam','Ham'])
 print(book[24])
